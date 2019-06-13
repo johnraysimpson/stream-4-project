c2ef5faf7977fe8f14f81846e8fb2b4ad8669455
John Simpson <johnnyraysimpson@hotmail.co.uk>
2019-06-13 20:33:29 +0000

    improved functionality of sign up and sign in, added some pages for testing purposes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+
+result = ""
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 app.config["MONGO_DBNAME"] = 'cook_book'
 app.config["MONGO_URI"] = 'mongodb+srv://jraysim:OrganD0NER@myfirstcluster-hdrnx.mongodb.net/cook_book?retryWrites=true&w=majority'
 
@@ -13,20 +16,39 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html", user=mongo.db.users.find_one({'first': 'John'}))
+    return render_template("home.html")
     
 @app.route('/sign_up')
 def sign_up():
     return render_template('signup.html', error = "")
     
-@app.route('/add_user', methods=["POST", "GET"])
+@app.route('/add_user/success')
+def add_user_success():
+    return render_template('signupsuccess.html')
+
+@app.route('/add_user/failed', methods=["POST", "GET"])
 def add_user():
     newuser = mongo.db.users.count({'username': request.form.get('username')})
-    if newuser > 0 :
-        return render_template('signup.html', error = "{0} has already been taken".format(request.form.get('username')))
-    else:
-        mongo.db.users.insert_one(request.form.to_dict())
-        return render_template('signupsuccess.html')
+    if request.method == 'POST':
+        if newuser > 0:
+            error = "{0} has already been taken".format(request.form.get('username'))
+        else:
+            return redirect(url_for('add_user_success'))
+    return render_template('signup.html', error=error)
+        
+@app.route('/sign_in_add')
+def sign_in_add():
+    return render_template('signinadd.html', error = "")
+    
+@app.route('/add_recipe', methods=["POST"])
+def log_in():
+    user = mongo.db.users.count({"$and": [{'username': request.form.get('username')}, {'password': request.form.get('password')}]})
+    if request.method == 'POST':
+        if user > 0:
+            return render_template('addrecipe.html', user = user)
+        else:
+            error = "Sorry, your Username or Password is incorrect"
+            return render_template('signinadd.html', error = error)
 
 
 if __name__ == '__main__':
