25287ffc352d62adad47c9abd15d1c166d7cc84b
John Simpson <johnnyraysimpson@hotmail.co.uk>
2019-06-14 14:07:58 +0000

    adapted functionality so once logged in user stays logged in unless they close the application

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -16,7 +16,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user="")
     
 @app.route('/sign_up')
 def sign_up():
@@ -33,22 +33,29 @@ def add_user():
         if newuser > 0:
             error = "{0} has already been taken".format(request.form.get('username'))
         else:
-            return redirect(url_for('add_user_success'))
+            flash("Your account has been created, log in to continue")
+            return render_template('signin.html')
     return render_template('signup.html', error=error)
         
-@app.route('/sign_in_add')
-def sign_in_add():
-    return render_template('signinadd.html', error = "")
+@app.route('/sign_in')
+def sign_in():
+    return render_template('signin.html', error = "")
     
-@app.route('/add_recipe', methods=["POST"])
+@app.route('/home/logged_in', methods=["POST"])
 def log_in():
-    user = mongo.db.users.count({"$and": [{'username': request.form.get('username')}, {'password': request.form.get('password')}]})
+    users = mongo.db.users.find({'username': request.form.get('username'), 'password': request.form.get('password')})
     if request.method == 'POST':
-        if user > 0:
-            return render_template('addrecipe.html', user = user)
+        if users.count() > 0:
+            user = mongo.db.users.find_one({'username': request.form.get('username'), 'password': request.form.get('password')})
+            return render_template('home.html', user=user)
         else:
             error = "Sorry, your Username or Password is incorrect"
-            return render_template('signinadd.html', error = error)
+            return render_template('signin.html', error = error)
+            
+@app.route('/home/<username>')
+def user_home(username):
+    user = mongo.db.users.find_one({'username': username})
+    return render_template('home.html', user=user)
 
 
 if __name__ == '__main__':
